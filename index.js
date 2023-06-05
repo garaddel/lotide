55ad917fce4cf7f7806151223bc0fbb2d7b1f648
garaddel <garaddel@gmail.com>
2023-06-05 19:11:36 -0400

    exporting all functions

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual')
+const eqArrays = require('./eqArrays');
+
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  eqArrays,
+};
